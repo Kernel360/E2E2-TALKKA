a3fc4beec788ab8a8ad82428980b56d158ae2167
song <shd2768@naver.com>
2024-07-31 15:48:17 +0900

    feat: #14 PR 리뷰내용 반영
    - @Table 삭제 및 @Column에 name 속성 추가
    - BIGINT(32) => BIGINT 변경
    - varchar 타입 columnDefinition 제거 및 length 추가
    - PK updatable 삭제
    - @EnableJpaAuditing 삭제
    - created_at, updated_at 오탈자 수정
    
    Closes #14

--- server/src/main/java/com/talkka/server/user/dao/UserEntity.java
+++ server/src/main/java/com/talkka/server/user/dao/UserEntity.java
@@ -13,7 +13,6 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,7 +20,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Entity
+@Entity(name = "users")
 @Getter
 @Setter
 @Builder
@@ -29,32 +28,31 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @EntityListeners(AuditingEntityListener.class)
-@Table(name = "users")
 public class UserEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_id", columnDefinition = "bigint(32)", nullable = false, updatable = false)
+	@Column(name = "user_id", nullable = false)
 	private Long userId;
 
-	@Column(name = "nickname", columnDefinition = "varchar(50)", nullable = false)
+	@Column(name = "nickname", length = 50, nullable = false)
 	private String nickname;
 
-	@Column(name = "oauth_provider", columnDefinition = "varchar(30)", nullable = false, updatable = false)
+	@Column(name = "oauth_provider", length = 30, nullable = false, updatable = false)
 	private String oauthProvider;
 
-	@Column(name = "access_token", columnDefinition = "varchar(255)", nullable = false)
+	@Column(name = "access_token", length = 255, nullable = false)
 	private String accessToken;
 
-	@Column(name = "grade", columnDefinition = "varchar(20)", nullable = false)
+	@Column(name = "grade", length = 20, nullable = false)
 	private String grade;
 
 	@CreatedDate
-	@Column(name = "create_at", columnDefinition = "datetime", nullable = false, updatable = false)
+	@Column(name = "created_at", nullable = false, updatable = false)
 	private LocalDateTime createAt;
 
 	@LastModifiedDate
-	@Column(name = "update_at", columnDefinition = "datetime", nullable = false)
+	@Column(name = "updated_at", nullable = false)
 	private LocalDateTime updateAt;
 
 	@Override
