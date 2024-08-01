1b80c01545667aa9b48ddf3cee286b0772c22f9a
song <shd2768@naver.com>
2024-08-01 20:33:49 +0900

    [FIX] BusRouteStationRepository에 JpaRepository extends 추가
    BusRouteStationRepository에 JpaRepository extends가 누락되어 추가

--- server/src/main/java/com/talkka/server/bus/dao/BusRouteStationRepository.java
+++ server/src/main/java/com/talkka/server/bus/dao/BusRouteStationRepository.java
@@ -1,7 +1,8 @@
 package com.talkka.server.bus.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BusRouteStationRepository {
+public interface BusRouteStationRepository extends JpaRepository<BusRouteStationEntity, Long> {
 }
