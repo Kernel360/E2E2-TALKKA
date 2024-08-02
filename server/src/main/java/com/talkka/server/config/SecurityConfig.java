ed4a902e39f71702ae2873825a319bc333f73571
PSH <gyak404@gmail.com>
2024-08-02 16:19:48 +0900

    feat: 네이버 인증 구현

--- server/src/main/java/com/talkka/server/config/SecurityConfig.java
+++ server/src/main/java/com/talkka/server/config/SecurityConfig.java
@@ -1,4 +1,38 @@
 package com.talkka.server.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.talkka.server.oauth.OAuth2LoginFailureHandler;
+import com.talkka.server.oauth.OAuth2LoginSuccessHandler;
+import com.talkka.server.oauth.service.CustomOAuth2Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final CustomOAuth2Service customOAuth2Service;
+	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+	private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/auth/**").permitAll()
+				.anyRequest().authenticated())
+			.oauth2Login(oauth -> oauth
+				.userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2Service))
+				.successHandler(oAuth2LoginSuccessHandler)
+				.failureHandler(oAuth2LoginFailureHandler)
+			);
+		return http.build();
+	}
 }
--- server/src/main/java/com/talkka/server/oauth/domain/AuthUserDto.java
+++ server/src/main/java/com/talkka/server/oauth/domain/AuthUserDto.java
@@ -1,4 +1,18 @@
 package com.talkka.server.oauth.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuthUserDto {
+	private String name;
+	private String email;
+	private String accessToken;
+	private String provider;
+	private Boolean isRegistered;
 }
