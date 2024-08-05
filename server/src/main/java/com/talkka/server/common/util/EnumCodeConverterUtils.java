7464d658c4cd10eefb6d1162ff15e308be884178
Photogrammer <khsj15@khu.ac.kr>
2024-08-05 18:10:14 +0900

    refactor: PR #40 리뷰 반영 - 변수 네이밍 변경

--- server/src/main/java/com/talkka/server/common/util/EnumCodeConverterUtils.java
+++ server/src/main/java/com/talkka/server/common/util/EnumCodeConverterUtils.java
@@ -1,13 +1,13 @@
 package com.talkka.server.common.util;
 
 public class EnumCodeConverterUtils {
-	public static <T extends Enum<T>> T fromCode(Class<T> enumType, String value) {
+	public static <T extends Enum<T>> T fromCode(Class<T> enumType, String codeString) {
 		for (T type : enumType.getEnumConstants()) {
-			if (type.toString().equals(value)) {
+			if (type.toString().equals(codeString)) {
 				return type;
 			}
 		}
-		throw new IllegalArgumentException("Invalid value: " + value);
+		throw new IllegalArgumentException("Invalid value: " + codeString);
 	}
 
 	public static <T extends Enum<T> & EnumCodeInterface> String toCode(T value) {
