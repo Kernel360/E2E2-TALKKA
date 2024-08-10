c78e7c29ce48c3f1f9402a7cedd16b267d9d4e09
Photogrammer <81505228+JuneParkCode@users.noreply.github.com>
2024-08-10 10:18:21 +0900

    [FEAT]: 소셜 로그인 과정 FE와 통합 (#51)
    - Security OAuth 경로 변경
    - AuthRole 로 refactor
    - UnregisteredUserFilter refactor
    
    Closes #47

--- server/src/main/java/com/talkka/server/oauth/service/CustomOAuth2Service.java
+++ server/src/main/java/com/talkka/server/oauth/service/CustomOAuth2Service.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import com.talkka.server.oauth.domain.NaverOAuth2User;
 import com.talkka.server.oauth.domain.OAuth2UserInfo;
+import com.talkka.server.oauth.enums.AuthRole;
 import com.talkka.server.user.dao.UserEntity;
 import com.talkka.server.user.dao.UserRepository;
 
@@ -20,15 +21,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2Service extends DefaultOAuth2UserService {
-
 	private final UserRepository userRepository;
 
 	// REFACTOR 반드시 필요함!
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-		// Spring Security가 auth provider(naver, kakao, google..)에게 인증 정보를 받아옴
-		// 이 시점에선 UNREGISTERED 권한 가지고 있음
-		// 만약 DB에 회원정보가 있으면 ROLE_USER 권한 새로 부여
 		OAuth2UserInfo oAuth2User = new NaverOAuth2User(super.loadUser(userRequest).getAttributes());
 		Map<String, Object> attributes = oAuth2User.getAttributes();
 		UserEntity user = userRepository.findByEmail(oAuth2User.getEmail()).orElse(null);
@@ -39,6 +36,6 @@ public class CustomOAuth2Service extends DefaultOAuth2UserService {
 		attributes.put("userId", user.getId());
 		attributes.put("nickname", user.getNickname());
 		return new NaverOAuth2User(attributes,
-			List.of(new SimpleGrantedAuthority("ROLE_USER")));
+			List.of(new SimpleGrantedAuthority(AuthRole.USER.getName())));
 	}
 }
