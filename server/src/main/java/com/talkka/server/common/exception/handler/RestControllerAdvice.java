7a603ead760ba0e9eb955e3a04856b00500ab4db
Photogrammer <khsj15@khu.ac.kr>
2024-08-16 15:20:25 +0900

    fix: RestControllerAdvice 에서 ApiResponse가 아닌 String으로 error message response 하도록 변경

--- server/src/main/java/com/talkka/server/common/exception/handler/RestControllerAdvice.java
+++ server/src/main/java/com/talkka/server/common/exception/handler/RestControllerAdvice.java
@@ -7,24 +7,27 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.talkka.server.common.dto.ApiRespDto;
+import com.talkka.server.common.exception.enums.InvalidEnumCodeException;
 import com.talkka.server.common.exception.http.HttpBaseException;
 
 @ControllerAdvice
 public class RestControllerAdvice {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ApiRespDto<Void>> handleMethodArgumentNotValidException(
+	public ResponseEntity<String> handleMethodArgumentNotValidException(
 		MethodArgumentNotValidException exception) {
-		ApiRespDto<Void> responseDto = ApiRespDto.<Void>builder()
-			.statusCode(400)
-			.message(exception.getBindingResult().getAllErrors().get(0).getDefaultMessage())
-			.build();
+		String message = exception.getBindingResult().getAllErrors().get(0).getDefaultMessage();
 
-		return new ResponseEntity<>(
-			responseDto,
-			HttpStatus.BAD_REQUEST
-		);
+		return ResponseEntity.badRequest().body(message);
 	}
 
+	@ExceptionHandler(InvalidEnumCodeException.class)
+	public ResponseEntity<String> handleInvalidEnumCodeException(InvalidEnumCodeException exception) {
+		String message = exception.getMessage();
+
+		return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@Deprecated
 	@ExceptionHandler(HttpBaseException.class)
 	public ResponseEntity<ApiRespDto<Void>> handleHttpException(HttpBaseException exception) {
 		ApiRespDto<Void> responseDto = ApiRespDto.<Void>builder()
@@ -37,5 +40,4 @@ public class RestControllerAdvice {
 			exception.getStatusCode()
 		);
 	}
-
 }
