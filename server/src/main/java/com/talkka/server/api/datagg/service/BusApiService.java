4a582a7e4cf5dbfca669b9f3a5052cfa03b0d49f
Photogrammer <81505228+JuneParkCode@users.noreply.github.com>
2024-08-20 12:48:44 +0900

    [FEAT] ApiClientException 추가 (#94)
    Closes #93

--- server/src/main/java/com/talkka/server/api/datagg/service/BusApiService.java
+++ server/src/main/java/com/talkka/server/api/datagg/service/BusApiService.java
@@ -2,19 +2,20 @@ package com.talkka.server.api.datagg.service;
 
 import java.util.List;
 
+import com.talkka.server.api.core.exception.ApiClientException;
 import com.talkka.server.api.datagg.dto.BusLocationBodyDto;
 import com.talkka.server.api.datagg.dto.BusRouteInfoBodyDto;
 import com.talkka.server.api.datagg.dto.BusRouteSearchBodyDto;
 import com.talkka.server.api.datagg.dto.BusRouteStationBodyDto;
 
 public interface BusApiService {
-	List<BusRouteSearchBodyDto> getSearchedRouteInfo(String routeName);
+	List<BusRouteSearchBodyDto> getSearchedRouteInfo(String routeName) throws ApiClientException;
 
-	List<BusRouteInfoBodyDto> getRouteInfo(String apiRouteId);
+	List<BusRouteInfoBodyDto> getRouteInfo(String apiRouteId) throws ApiClientException;
 
-	List<BusRouteStationBodyDto> getRouteStationInfo(String apiRouteId);
+	List<BusRouteStationBodyDto> getRouteStationInfo(String apiRouteId) throws ApiClientException;
 
-	List<BusLocationBodyDto> getBusLocationInfo(String apiRouteId);
+	List<BusLocationBodyDto> getBusLocationInfo(String apiRouteId) throws ApiClientException;
 
 	// List<RouteBusStationArrivalInfoRespDto> getBusStationArrivalInfo(String routeId);
 }
