6608cce91d90cd0ed0fe57f2df70ed8b70948b4d
Haedeok <shd2768@naver.com>
2024-08-20 12:50:21 +0900

    refactor: Repository findByApiXXXId 롤백

--- server/src/main/java/com/talkka/server/bus/dao/BusRouteRepository.java
+++ server/src/main/java/com/talkka/server/bus/dao/BusRouteRepository.java
@@ -1,6 +1,7 @@
 package com.talkka.server.bus.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BusRouteRepository extends JpaRepository<BusRouteEntity, Long> {
 	boolean existsByApiRouteId(String apiRouteId);
+
+	Optional<BusRouteEntity> findByApiRouteId(String apiRouteId);
 
 	List<BusRouteEntity> findAllByRouteNameStartingWithOrderByRouteNameAsc(String routeName);
 }
