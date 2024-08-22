40236a12f9655667c7865036caf61432648a16a9
Photogrammer <khsj15@khu.ac.kr>
2024-08-22 11:51:48 +0900

    feat: path 기본 구조

---
+++ client/app/path/components/Bookmark.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+interface BookmarkProps {
+  bookmark: any
+}
+
+export default function Bookmark({ bookmark }: BookmarkProps) {
+  // User 의 Bookmark 중 첫 번째를 보여줍니다. 없을 경우 빈 화면을 보여줍니다.
+  return <div></div>
+}
