3f6907fffbb19c8b5376908c215e00ab18a3dc64
PSH <gyak404@gmail.com>
2024-08-23 22:31:56 +0900

    feat : 버스 위치 데이터 가공을 위한 메소드 추가

--- server/src/main/java/com/talkka/server/bus/dao/BusLocationRepository.java
+++ server/src/main/java/com/talkka/server/bus/dao/BusLocationRepository.java
@@ -1,8 +1,19 @@
 package com.talkka.server.bus.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
- 
+
 @Repository
 public interface BusLocationRepository extends JpaRepository<BusLocationEntity, Long> {
+
+	@Query(value = "select distinct l.apiCallNo from bus_location l")
+	List<Integer> getDistinctApiCallNoList();
+
+	@Query(value = "select count(*) from bus_location  l")
+	Integer getRowNum();
+
+	List<BusLocationEntity> findByApiCallNo(Integer apiCallNo);
 }
