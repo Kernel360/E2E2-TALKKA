cf2af2b18745192c8b29608cb04dc43614ca4925
PSH <gyak404@gmail.com>
2024-08-26 00:46:35 +0900

    feat : 노선-정거장 별 북마크 수 집계 쿼리 추가

--- server/src/main/java/com/talkka/server/bookmark/dao/BookmarkDetailRepository.java
+++ server/src/main/java/com/talkka/server/bookmark/dao/BookmarkDetailRepository.java
@@ -1,11 +1,17 @@
 package com.talkka.server.bookmark.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookmarkDetailRepository extends JpaRepository<BookmarkDetailEntity, Long> {
 	List<BookmarkDetailEntity> findByBookmarkId(Long bookmarkId);
 
 	List<BookmarkDetailEntity> deleteByBookmarkId(Long bookmarkId);
+
+	@Query("SELECT b.busRouteStationId AS key, COUNT(b) AS value FROM bookmark_detail b GROUP BY b.busRouteStationId HAVING COUNT(b) > 0")
+	Map<Long, Integer> countGroupedByBusRouteStationId();
+
 }
