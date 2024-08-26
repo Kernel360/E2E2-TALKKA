af3a386e4df4babf5543c6f1d25bfa9277eb4db2
Photogrammer <khsj15@khu.ac.kr>
2024-08-26 15:46:26 +0900

    feat: 버스 실시간 정보 API 구현

---
+++ server/src/main/java/com/talkka/server/bus/controller/BusLiveInfoController.java
@@ -0,0 +1,34 @@
+package com.talkka.server.bus.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.talkka.server.bus.exception.BusRouteStationNotFoundException;
+import com.talkka.server.bus.exception.GetBusLiveArrivalInfoFailedException;
+import com.talkka.server.bus.service.BusLiveInfoService;
+import com.talkka.server.common.dto.ErrorRespDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/bus/live")
+public class BusLiveInfoController implements BusLiveInfoApi {
+	private final BusLiveInfoService busLiveInfoService;
+
+	@Override
+	@GetMapping("/{routeStationId}")
+	public ResponseEntity<?> getBusLiveInfo(@PathVariable Long routeStationId) {
+		ResponseEntity<?> response;
+		try {
+			var busLiveInfo = busLiveInfoService.getBusLiveInfo(routeStationId);
+			response = ResponseEntity.ok(busLiveInfo);
+		} catch (BusRouteStationNotFoundException | GetBusLiveArrivalInfoFailedException exception) {
+			response = ResponseEntity.badRequest().body(ErrorRespDto.of(exception));
+		}
+		return response;
+	}
+}
