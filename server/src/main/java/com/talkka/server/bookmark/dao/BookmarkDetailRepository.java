2f14b2217ad472c8e53cabc88fac56d0213b14d1
Photogrammer <khsj15@khu.ac.kr>
2024-08-28 14:40:31 +0900

    refactor: BookmarkDetailRepository 불필요한 method 삭제

--- server/src/main/java/com/talkka/server/bookmark/dao/BookmarkDetailRepository.java
+++ server/src/main/java/com/talkka/server/bookmark/dao/BookmarkDetailRepository.java
@@ -1,13 +1,6 @@
 package com.talkka.server.bookmark.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkDetailRepository extends JpaRepository<BookmarkDetailEntity, Long> {
-	List<BookmarkDetailEntity> findByBookmarkId(Long bookmarkId);
-
-	void deleteByBookmark(BookmarkEntity bookmark);
-
-	List<BookmarkDetailEntity> deleteByBookmarkId(Long bookmarkId);
 }
