e9ae90aea014ec945c22ba2a5f5eec1d1e4ed537
Photogrammer <khsj15@khu.ac.kr>
2024-08-28 16:21:34 +0900

    refactor: 사용하지 않는 Exception 제거

--- server/src/main/java/com/talkka/server/common/exception/handler/RestControllerAdvice.java
+++ server/src/main/java/com/talkka/server/common/exception/handler/RestControllerAdvice.java
@@ -7,10 +7,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.talkka.server.common.dto.ApiRespDto;
 import com.talkka.server.common.dto.ErrorRespDto;
-import com.talkka.server.common.exception.enums.InvalidEnumCodeException;
-import com.talkka.server.common.exception.http.HttpBaseException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,12 +22,6 @@ public class RestControllerAdvice {
 		String message = exception.getBindingResult().getAllErrors().get(0).getDefaultMessage();
 
 		return ResponseEntity.badRequest().body(ErrorRespDto.of(message));
-	}
-
-	@ExceptionHandler(InvalidEnumCodeException.class)
-	public ResponseEntity<ErrorRespDto> handleInvalidEnumCodeException(InvalidEnumCodeException exception) {
-		log.error("InvalidEnumCodeException: {}", exception.getMessage());
-		return new ResponseEntity<>(ErrorRespDto.of(INTERNAL_SERVER_ERROR_MESSAGE), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(AuthorizationDeniedException.class)
@@ -50,19 +41,5 @@ public class RestControllerAdvice {
 			exception.getStackTrace()
 		);
 		return new ResponseEntity<>(ErrorRespDto.of(INTERNAL_SERVER_ERROR_MESSAGE), HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-
-	@Deprecated
-	@ExceptionHandler(HttpBaseException.class)
-	public ResponseEntity<ApiRespDto<Void>> handleHttpException(HttpBaseException exception) {
-		ApiRespDto<Void> responseDto = ApiRespDto.<Void>builder()
-			.statusCode(exception.getStatusCode().value())
-			.message(exception.getMessage())
-			.build();
-
-		return new ResponseEntity<>(
-			responseDto,
-			exception.getStatusCode()
-		);
 	}
 }
