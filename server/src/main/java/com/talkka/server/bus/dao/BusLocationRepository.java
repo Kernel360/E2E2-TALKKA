d0a725ad6966f5f174eaee4d4d60d76c17bfbd65
Photogrammer <81505228+JuneParkCode@users.noreply.github.com>
2024-08-28 17:20:19 +0900

    [FIX] Schema 일부 누락으로 인한 오류 수정 (#133)
    ## 작업 내용
    - Schema.sql 삭제
    - data.sql 을 통해서 필요한 정적 데이터 로드
    
    ---------
    
    Co-authored-by: PSH <gyak404@gmail.com>

--- server/src/main/java/com/talkka/server/bus/dao/BusLocationRepository.java
+++ server/src/main/java/com/talkka/server/bus/dao/BusLocationRepository.java
@@ -1,23 +1,8 @@
 package com.talkka.server.bus.dao;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BusLocationRepository extends JpaRepository<BusLocationEntity, Long> {
-
-	@Query(value = "select distinct l.apiCallNo from bus_location l")
-	List<Integer> getDistinctApiCallNoList();
-
-	@Query(value = "select count(*) from bus_location")
-	Integer getRowNum();
-
-	List<BusLocationEntity> findByApiCallNo(Integer apiCallNo);
-
-	List<BusLocationEntity> findByCreatedAtBetween(LocalDateTime startTime, LocalDateTime endTime);
-
 }
