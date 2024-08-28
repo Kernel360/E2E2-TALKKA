3513910b6b465d8198966b67895294cbef9ceb97
PSH <gyak404@gmail.com>
2024-08-28 15:19:53 +0900

    refactor : standardTime 계산 코드 명확하게 변경

--- server/src/main/java/com/talkka/server/bus/dto/BusRemainSeatDto.java
+++ server/src/main/java/com/talkka/server/bus/dto/BusRemainSeatDto.java
@@ -24,14 +24,17 @@ public class BusRemainSeatDto {
 	private LocalDateTime standardTime;
 	private final List<SeatInfo> remainSeatList = new ArrayList<>();
 
-	public BusRemainSeatDto(List<BusRemainSeatEntity> seats) {
-		for (var seat : seats) {
+	public BusRemainSeatDto(List<BusRemainSeatEntity> seatList) {
+		for (var seat : seatList) {
 			plateType = seat.getPlateType();
 			plateNo = seat.getPlateNo();
 			remainSeatList.add(new SeatInfo(seat.getCreatedAt(), seat.getEmptySeat()));
 		}
-		if (!seats.isEmpty()) {
-			standardTime = seats.get(seats.size() >> 1).getCreatedAt();
+		if (!seatList.isEmpty()) {
+			// 기준 정거장을 기준으로 양 옆으로 조회하기 때문에
+			// 기준 정거장은 배열의 정 가운데에 위치함
+			int middleIdx = seatList.size() / 2;
+			standardTime = seatList.get(middleIdx).getCreatedAt();
 		}
 	}
 }
