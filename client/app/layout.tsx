51d775b04d4fe2025f33606c33af50bb598b39e8
Photogrammer <81505228+JuneParkCode@users.noreply.github.com>
2024-08-29 11:31:58 +0900

    [FIX] FE 빌드 에러 수정 (#142)
    ## 작업 내용
    - 빌드 중 오류 발생하는 부분 제거 / 수정

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -8,10 +8,6 @@ import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
-
-
-
-
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
@@ -23,7 +19,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   icons: {
-    icon: "/favicon.ico",
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🚎</text></svg>",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
